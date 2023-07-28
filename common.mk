ba76a801894f8ad537fabc9995e177ac3f9f9ee3
Simon1511 <me@simon1511.de>
2023-07-28 13:02:56 +0200

    s5e8825-common: Remove custom UdfpsHandler
    It's not needed anymore
    
    Change-Id: I0ad954ae37a4c3d6ccb05d6dfa65b372eccbc282
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -159,8 +159,7 @@ endif
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service-samsung.s5e8825 \
-    sensors.samsung \
-    UdfpsHandler_s5e8825
+    sensors.samsung
 endif
 
 # Gatekeeper
