3a55a82908c82f06acedadbcaf48f84e9aaa066a
ChrisCatto <chriscatto@proton.me>
2025-06-14 07:48:51 +0000

    s5e8825-common: Refactor keymint service patching
    * There is an issue with keymint V4 NDK in our device because of some
      changes to keystore2 related to setAdditionalAttestationInfo and
      after Android 16 release and it seems like we cannot boot by keep
      using it. Downgrading keymint NDK to V3 allows us to boot again.
    
    * However, by doing this, checkelf must be disabled for keymint service
      the same goes for dependencies generation in Android.bp (for this
      service only).
    
    Change-Id: Iefdb624502ca9bd78881c0e542a2a8f5584a8be7
    Signed-off-by: ChrisCatto <chriscatto@proton.me>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -185,6 +185,18 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor \
+    libkeymaster_portable.vendor
+
+# Keymint
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint-V3-ndk.vendor \
+    lib_android_keymaster_keymint_utils.vendor \
+    libcppbor_external.vendor \
+    libkeymint.vendor
+
 # Kernel Modules
 PRODUCT_PACKAGES += \
     toolbox.vendor_ramdisk
