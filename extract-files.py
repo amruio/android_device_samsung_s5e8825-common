d575eac2d4048f6bbb81bd860778052b40a90f2c
Flopster101 <nahuelgomez329@gmail.com>
2025-07-01 19:36:40 -0300

    s5e8825-common: Switch to prebuilt Exynos C2 stack
    Also patch the manifest in extract-files to remove the default0 instance, as
    this causes init to freak out and break mediaserver.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -63,6 +63,11 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libshim_sensorndkbridge.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
+    'vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_0.xml': blob_fixup()
+        .regex_replace(
+            r'(?m)^[ \t]*<instance>default0</instance>[ \t]*(?:\r?\n)+',
+            ''
+        ),
     (
         'vendor/lib/sensors.grip.so',
         'vendor/lib/sensors.inputvirtual.so',
