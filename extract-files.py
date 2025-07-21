0197c9205aacdbd2c6c5f8009ae08e7edb2f7e3b
Flopster101 <nahuelgomez329@gmail.com>
2025-07-21 16:01:50 -0300

    s5e8825-common: Disable libsec-ril patch for now
    It doesn't work for all carriers, like mine for example (Telecom Personal AR).
    
    We'll use the framework-side patch from Phh, which can be found here:
    https://github.com/TrebleDroid/platform_frameworks_opt_telephony/commits/9de26ca6b0fcbbb8bc27ca75a57697a7724f797d/src/java/com/android/internal/telephony/MessagingResponse.java
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -85,9 +85,9 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/lib64/libssl-tm.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-tm.so'),
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
-            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
+    # 'vendor/lib64/libsec-ril.so': blob_fixup()
+    #     .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
+    #         '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
     'vendor/lib/soundfx/libswdap.so': blob_fixup()
         .sig_replace('30 46 88 47 07 46 30 68',
                      '30 46 88 47 01 27 30 68'),
