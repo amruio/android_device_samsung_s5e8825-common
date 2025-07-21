5885c051cc0d40a29a3ace9dfd2397588f2e5921
Flopster101 <nahuelgomez329@gmail.com>
2025-07-21 16:01:50 -0300

    s5e8825-common: Disable libsec-ril patch for now
    It doesn't work for all carriers, like mine for example (Telecom Personal AR).
    
    We'll use the framework-side patch from Phh, which can be found here:
    https://github.com/TrebleDroid/platform_frameworks_opt_telephony/commits/9de26ca6b0fcbbb8bc27ca75a57697a7724f797d/src/java/com/android/internal/telephony/MessagingResponse.java
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -88,9 +88,9 @@ blob_fixups: blob_fixups_user_type = {
                     '1F 20 03 D5 A9 02 80 52 29 80 01 B9 08 65 40 B9')
         .sig_replace('E0 17 9F 1A FD 7B C2 A8 C0 03 5F D6',
                     '20 00 80 52 FD 7B C2 A8 C0 03 5F D6'),
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
-            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
+    # 'vendor/lib64/libsec-ril.so': blob_fixup()
+    #     .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
+    #         '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
     'vendor/lib64/libssl-tm.so': blob_fixup()
