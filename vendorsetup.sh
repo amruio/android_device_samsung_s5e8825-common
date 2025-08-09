e8cd945fb28b8b7319f36dcb8275623e9a1de237
Ksawlii <maja.blaszczak@proton.me>
2025-08-09 22:55:31 +0200

    s5e8825-common: vendorsetup: burp
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,13 @@
-if [ -d "hardware/samsung/doze" ]; then
+if [[ -d "hardware/samsung/doze" ]]; then
     rm -rf "hardware/samsung/doze"
 fi
 
-if [ -d "hardware/samsung/AdvancedDisplay" ]; then
+if [[ -d "hardware/samsung/AdvancedDisplay" ]]; then
     rm -rf "hardware/samsung/AdvancedDisplay"
 fi
 
-if [ -d "system/nfc" ] && ! grep "nfa_t4tnfcee_is_config" system/nfc/src/nfa/include/nfa_nfcee_int.h > /dev/null; then
+if [[ -d "system/nfc" ]] && ! grep -q "nfa_t4tnfcee_is_config" system/nfc/src/nfa/include/nfa_nfcee_int.h; then
   cd system/nfc
-  git fetch https://github.com/LineageOS/android_system_nfc refs/changes/55/423355/1 && git cherry-pick FETCH_HEAD
-  cd -
+  git fetch https://github.com/LineageOS/android_system_nfc refs/changes/55/423355/1 && git cherry-pick FETCH_HEAD >/dev/null
+  cd - &> /dev/null
 fi
