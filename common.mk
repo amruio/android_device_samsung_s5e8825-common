8225ab5634d1ddb0b6a9fa602be902ccb62d9f85
Ksawlii <maja.blaszczak@proton.me>
2025-08-11 12:35:11 +0200

    s5e8825-common: Switch to AIDL Fingerprint HAL
    * For UDFPS devices
    
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -149,14 +149,11 @@ PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
 
 # Fingerprint
-ifneq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.samsung
-endif
 
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service-samsung.s5e8825 \
     sensors.samsung \
     UdfpsHandler_s5e8825
 endif
