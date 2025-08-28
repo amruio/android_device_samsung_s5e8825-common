eb7d736fe9bc6ee96abdd17041b817d02ac6c6fe
Flopster101 <nahuelgomez329@gmail.com>
2025-08-28 02:56:17 -0300

    Revert "s5e8825-common: Migrate to Lineage Health IFastCharge"
    Pixelage does not yet implement this.
    
    This reverts commit 456a62e70961ec1795e7e7c43bebb6cf174336cc.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.samsung
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.s5e8825
@@ -188,10 +192,6 @@ PRODUCT_PACKAGES += \
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
-$(call soong_config_set,lineage_health,fast_charge_value_none,1)
-$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # Media
 PRODUCT_COPY_FILES += \
