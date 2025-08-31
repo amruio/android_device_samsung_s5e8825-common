5bfc591aae6909b1d84e019b2ff9e826cddc8e11
Flopster101 <nahuelgomez329@gmail.com>
2025-08-31 03:51:41 -0300

    Revert "s5e8825-common: releasetools: Flash recovery included in OTA zip"
    bad.
    
    This reverts commit a9dcc3e780626ad816c6adfc6f23ff04c69bd23d.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -49,7 +49,6 @@ def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vendor_boot.img", "/dev/block/by-name/vendor_boot")
-  AddImage(info, "recovery.img", "/dev/block/by-name/recovery")
 
   if "RADIO/models" in info.input_zip.namelist():
     modelsIncluded = []
